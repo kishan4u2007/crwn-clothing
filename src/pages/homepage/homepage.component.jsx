e734d2f32f886ea41379eb647ec5211aa14fbecb
Kishan Raikwar <kishan4u2007@gmail.com>
2019-08-19 23:59:17 +0530

    adding project

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Directory from '../../components/directory/direcotry.component';
+import './homepage.style.scss';
+
+
+
+const HomePage = () => (
+    <div className="homepage">
+        <Directory />
+    </div>
+)
+
+export default HomePage;
